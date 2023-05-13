af3d04d89ccc1b0e6ae063f98a1da0001c22336f
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-05-13 00:10:20 -0700

    test review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" """
+""" Unittest module for the Review class """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ Tests cases for the Review class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initializes the object attributes of the Review class"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ Tests the functionality of the place id """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ Tests the functionality of the user id  """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ Verifies the behaviour of the text attribute of the object place"""
         new = self.value()
         self.assertEqual(type(new.text), str)
+
+if __name__ = "__main__"
+    unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" """
+""" Unittest module for the State class"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ Tests cases for the State class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initializes the object attributes """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """ Tests new name of the State class """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+if __name__ = "__main__"
+    unittest.main()
